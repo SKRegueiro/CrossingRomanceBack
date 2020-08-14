8f5dacb4fc399c58628b26adb9135f8e5ca771d5
Sergio <scaminoregueiro@gmail.com>
2020-08-14 08:13:11 +0200

    First commit to heroku

---
+++ config/keys.js
@@ -0,0 +1,22 @@
+module.exports = {
+    google: {
+        CLIENT_ID: "528831353918-9p8bnpgpsld9mf41pt07u8tple2mefd8.apps.googleusercontent.com",
+        CLIENT_SECRET: "f7DkYI0_LP8JG5oFwDMG9dN2"
+    },
+    facebook: {
+        CLIENT_ID: "2702525040070885",
+        SECRET_ID: "77cdc4c45a25777614a8c5e1d424a46c"
+    },
+    twitter: {
+        CONSUMER_KEY: "zcyV3QoYLht23rdOB5WJCS6Ul",
+        CONSUMER_SECRET: "TRAvkgNHj436Glfm9GIEaN7yp6t2UYXyzNhTaURXyDrtWt6wgX"
+    },
+    session: {
+        cookieKey: 'asdf098ahdsf9fhdsakj93dkfb4b52',
+        cookieSecret: 'dsaf832498ydfsn4982ds93d1fe'
+    },
+    aws: {
+        accessKeyId: "AKIAIPGDCWI353EPNXRA",
+        secretAccessKey: "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"
+    }
+}
