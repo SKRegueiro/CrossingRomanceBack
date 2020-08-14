fa16b36258f770b146b46fe53d79de58e8c1b994
Sergio <scaminoregueiro@gmail.com>
2020-08-14 19:32:45 +0200

    minor change

--- app.js
+++ app.js
@@ -23,14 +23,29 @@ app.set('view engine', 'pug');
 app.use(helmet())
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors({
-  origin: 'https://crossingpaths.netlify.app/',
-  credentials: true
-}));
-aws.config.update({
-  accessKeyId: keys.aws.accessKeyId,
-  secretAccessKey: keys.aws.secretAccessKey
-})
+
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.use(allowCrossDomain)
+// app.use(cors({
+//   origin: 'https://crossingpaths.netlify.app/',
+//   credentials: true
+// }));
+// aws.config.update({
+//   accessKeyId: keys.aws.accessKeyId,
+//   secretAccessKey: keys.aws.secretAccessKey
+// })
 
 
 
