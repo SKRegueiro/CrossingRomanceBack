3ac8aba8c25b4241839d1ce8523559022c4fbf72
Sergio <scaminoregueiro@gmail.com>
2020-08-28 10:27:29 +0200

    minot change

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.use(passport.session());
 
 
 const isLoggedIn = (req, res, next) => {
-  console.log(req.user)
   if (req.user) {
     global.loggedUser = req.user[0]
     next()
@@ -68,6 +67,7 @@ app.use('/', (req, res, next) => {
   console.log(req.user);
   next()
 })
+
 app.use('/auth', authRouter);
 app.use('/users',
   isLoggedIn,
