966a7c575f15e423de1a47e88bbc4aff68235ca9
Sergio <scaminoregueiro@gmail.com>
2020-08-28 12:11:22 +0200

    minor change

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(cors({
   credentials: true
 }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://crossingpathsserver.herokuapp.com/");
+  res.header("Access-Control-Allow-Origin", "https://crossingpaths.netlify.app/");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 aws.config.update({
   accessKeyId: keys.aws.accessKeyId,
   secretAccessKey: keys.aws.secretAccessKey
