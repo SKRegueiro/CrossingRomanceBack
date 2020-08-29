12f22eb8bbf4a430144448cbee3be8d2da7a2b29
Sergio <scaminoregueiro@gmail.com>
2020-08-29 11:03:17 +0200

    minor change

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.use(cookieSession({
   name: 'crossing-session',
   key: [keys.session.cookieKey],
   secret: keys.session.cookieSecret,
-  // httpOnly: true,
-  // secure: false,
-  // SameSite: 'none',
+  httpOnly: true,
+  secure: false,
+  SameSite: 'none',
   // domain: 'crossingpaths.netlify.app',
   maxAge: 24 * 60 * 60 * 1000
 }));
