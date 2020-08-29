273819dbd147d96e6eb2e5c0fac4f533c97db55e
Sergio <scaminoregueiro@gmail.com>
2020-08-29 18:22:56 +0200

    minor change

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const googleRouter = require('./authentication/googleRouter');
 const facebookRouter = require('./authentication/facebookRouter');
 const twitterRouter = require('./authentication/twitterRouter')
-const instagramRouter = require('./authentication/instagramRouter')
+const instagramRouter = require('./authentication/instagramRouter');
+const { registerProfile } = require('../models/usersModels');
 
 
 router.use('/google', googleRouter)
@@ -14,8 +15,9 @@ router.use('/twitter', twitterRouter)
 router.use('instagram', instagramRouter)
 
 router.get('/success', (req, res) => {
-    // res.redirect('https://crossingpaths.netlify.app/')
-    res.redirect('http://localhost:8080/')
+    res.redirect('https://crossingpaths.netlify.app/')
+    // res.redirect('http://localhost:8080/')
+
 })
 router.get('/failure', (req, res) => res.send('You failed to log in!'))
 
